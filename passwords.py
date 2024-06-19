291328b7aac3aac4854afe16f61fa78800e70d07
chikati chandini <chandinicheekati@gmail.com>
2024-06-19 17:55:30 +0530

    Add files via upload

---
+++ passwords.py
@@ -0,0 +1,26 @@
+username1='chandini'
+password1='6303669593'
+username=input("enter username")
+password=input("enter password")
+if username==username1 and password==password1:
+    print("you entered correct")
+else:
+    print("This is your first attempt")
+    username=input("enter username")
+    password=input("enter password")
+    if username==username1 and password==password1:
+        print("password is correct")
+    else:
+        print("This is your second attempt")
+        username=input("enter username")
+        password=input("enter password")
+        if username==username1 and password==password1:
+            print("correct")
+        else:
+            print("This is your last attempt")
+            username=input("enter username")
+            password=input("enter password")
+            if username==username1 and password==password1:
+                print("correct")
+            else:
+                print("attemts are over")
