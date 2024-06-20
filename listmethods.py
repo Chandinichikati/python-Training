c250cac11bdd6760e13f6491adaf24c792c19829
chikati chandini <chandinicheekati@gmail.com>
2024-06-20 14:31:21 +0530

    Add files via upload

---
+++ listmethods.py
@@ -0,0 +1,8 @@
+l=[7,7.7,True,"chandu","swapna"]
+l.append("neelima")
+l.extend([1, 2, 3])
+l.insert(2,"vanaja")
+l.remove(True)
+l.pop(-2)
+l.clear()
+print(l)
---
+++ listmethods2.py
@@ -0,0 +1,4 @@
+l=["neelima","vanaja","bhanu","chandu","swapna","chandu"]
+print(l.index("chandu"))
+print(l.count("chandu"))
+print(l.copy())
