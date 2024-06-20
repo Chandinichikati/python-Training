d79841bf9f14abae40a4ec85d1e62349055b1621
chikati chandini <chandinicheekati@gmail.com>
2024-06-20 14:18:53 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,17 @@
+s=[10,True,5.45,"hello"]
+print(s)
+l=input("enter the no:").split("_")
+print(l)
+l=list(map(int,input("enter the no:").split("_")))
+print(l)
+l=list(map(int,"123"))
+print(l)
+l=input().split()
+print(l)
+c=[7,7.7,"python",(5,7,3)]
+print(c)
+l=[7,7.7,True,"chandu","swapna"]
+print(l)
+l=[7,7.7,True,"chandu","swapna"]
+del l[2]
+print(l)
